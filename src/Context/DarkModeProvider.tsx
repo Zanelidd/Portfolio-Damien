b43fcae1dddae84faadf3f6e4dcf88a597e3e59c
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-12-23 18:29:15 +0100

    Correction type error

--- src/Context/DarkModeProvider.tsx
+++ src/Context/DarkModeProvider.tsx
@@ -1,7 +1,17 @@
-import { createContext } from "react";
-import { DarkModeContextType } from "../services/types";
+import { createContext, useState } from "react";
+import { ChildrenProps, DarkModeContextType } from "../services/types";
 
 export const DarkModeContext = createContext<DarkModeContextType>({
   darkMode: false,
   setDarkMode: () => {},
 });
+
+export function DarkModeProvider({ children }: ChildrenProps) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import { DarkModeContext } from "../Context/DarkModeProvider";
 import ProjectCards from "../Components/ProjectCards";
 import style from "./styles/Projects.module.css";
-import project from "../Datas/Project";
+import projects from "../Datas/Project";
 import { DarkModeContextType, ProjectType } from "../services/types";
 import { useContext } from "react";
 
@@ -18,8 +18,8 @@ const Projects = () => {
       >
         <h1 className={darkMode ? style.title_dark : style.title}>Projets</h1>
         <div className={style.project_cards}>
-          {project?.map((proje: ProjectType) => {
-            return <ProjectCards key={proje.id} proje={proje} />;
+          {projects?.map((project: ProjectType) => {
+            return <ProjectCards key={project.id} project={project} />;
           })}
         </div>
       </div>
